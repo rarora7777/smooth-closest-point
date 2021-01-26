d3cf0ebfaeff4972b9d2264ae51d3ede673f0713
Rahul Arora <arorar@cs.toronto.edu>
2021-01-26 18:42:07 -0500

    Minor

--- computeAndSaveOffsetSurfaces.m
+++ computeAndSaveOffsetSurfaces.m
@@ -1,5 +1,5 @@
 function computeAndSaveOffsetSurfaces(modelName)
-    [V, F] = readOBJ(['./data/' modelName '.obj']);
+    [V, F] = readOBJ("./data/" + modelName + ".obj");
     
     offset = @(V) 0.05*max(1, norm(max(V)-min(V)));
     
@@ -12,13 +12,13 @@ function computeAndSaveOffsetSurfaces(modelName)
     [V1, IV1] = remove_unreferenced(V1, F1);
     F1 = IV1(F1);
     
-    writeOBJ(['./data/' modelName '_out_cgal.obj'], V1, F1);
+    writeOBJ("./data/" + modelName + "_out_cgal.obj", V1, F1);
     [V2, F2] = signed_distance_isosurface(V, F, 'SignedDistanceType', 'pseudonormal', 'Level', -offset(V));
     
     if numel(F2)==0
         disp('In-mesh is empty.');
     else
-        writeOBJ(['./data/' modelName '_in_cgal.obj'], V2, F2);
+        writeOBJ("./data/" + modelName + "_in_cgal.obj", V2, F2);
     end
     
     disp('Saved offset surfaces. Use TetWild to improve mesh quality, and then run computeAndSaveEmbedding');
