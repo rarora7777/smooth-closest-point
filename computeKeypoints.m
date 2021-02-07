7aacc2875e370aa3344e21e7d436c149ebf94054
Rahul Arora <arorar@cs.toronto.edu>
2021-02-07 15:28:16 -0500

    Added bspline

--- computeKeypoints.m
+++ computeKeypoints.m
@@ -3,17 +3,12 @@ function kpi = computeKeypoints(V, F, curve, scale)
     minLength = 0.03 / scale;   % 3 cm
     maxLength = 0.15 / scale;   % 15 cm
     
-%     fIdx = curve.FI;
-%     bary = curve.B;
-%     bary(bary < 0) = bary;
-%     bary = bary ./ sum(bary, 2);
-    
+
     [gk, ~, path] = geodesicCurvature(V, F, curve);
     P = [vertcat(cell2mat(path).x), vertcat(cell2mat(path).y) vertcat(cell2mat(path).z)];
     
     edge = diff(P, [], 1);
     eLen = vecnorm(edge, 2, 2);
-%     vLen = ([0; eLen] + [eLen; 0])/2;
     L = sum(eLen);
     cumLen = [0; cumsum(eLen)];
     minLen = max(L/n, minLength);
@@ -25,11 +20,8 @@ function kpi = computeKeypoints(V, F, curve, scale)
     
     [peakVal, candidates] = findpeaks(signal, 'MinPeakHeight', mean(signal));
     
-%     midPt = 1+find(cumLen > L/2, 1);
     kpi = [1; numel(gk)];
     
-%     peakVal(candidates <= midPt) = [];
-%     candidates(candidates <= midPt) = [];
     
     % sort candidates by the peak values
     [~, idx] = sort(peakVal, 'descend');
@@ -44,20 +36,7 @@ function kpi = computeKeypoints(V, F, curve, scale)
         end
     end
     
-%     prevPt = midPt;
-%     kpi = midPt;
-%     
-%     while cumLen(prevPt) < L*(n-1)/n
-%         cdIdx = find(cumLen(candidates) > cumLen(prevPt) + L/n, 1);
-%         if isempty(cdIdx)
-%             break;
-%         end
-%         kpi = [kpi; candidates(cdIdx)];
-%         prevPt = kpi(end);
-%     end
-    
-%     kpi = [1; kpi; numel(gk)];
-    
+
     kpi = sort(kpi)
     while 1
         lenDiff = diff(cumLen(kpi));
